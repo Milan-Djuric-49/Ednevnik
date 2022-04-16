f3a30bfd5b0058d0ed85a5b2675aa3c0c946e9c5
Milan-Djuric <milan.djuric@prvabeogim.edu.rs>
2022-04-16 22:44:20 +0200

    Ocena design + combo

---
+++ Ednevnik/Ocena.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Ednevnik
+{
+    public partial class Ocena : Form
+    {
+        public Ocena()
+        {
+            InitializeComponent();
+        }
+
+        private void Ocena_Load(object sender, EventArgs e)
+        {
+            cmb_GodinaPopulate();
+            cmb_Predmet.Enabled = false;
+            cmb_Odeljenje.Enabled = false;
+            cmb_Ucenik.Enabled = false;
+            cmb_Ocena.Items.Add(1);
+            cmb_Ocena.Items.Add(2);
+            cmb_Ocena.Items.Add(3);
+            cmb_Ocena.Items.Add(4);
+            cmb_Ocena.Items.Add(5);
+            cmb_Ocena.Enabled = false;
+            cmb_ProfesorPopulate();
+        }
+
+        private void cmb_GodinaPopulate()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Skolska_godina", veza);
+            DataTable dt_godina = new DataTable();
+            adapter.Fill(dt_godina);
+            cmb_Godina.DataSource = dt_godina;
+            cmb_Godina.ValueMember = "id";
+            cmb_Godina.DisplayMember = "naziv";
+            //cmb_Godina.SelectedValue = 2;
+        }
+        private void cmb_Godina_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if(cmb_Godina.IsHandleCreated && cmb_Godina.Focused)
+            {
+                cmb_ProfesorPopulate();
+            }
+        }
+        
+        private void cmb_ProfesorPopulate()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            StringBuilder naredba = new StringBuilder("SELECT DISTINCT Osoba.id, ime + ' ' + prezime AS naziv FROM Osoba ");
+            naredba.Append(" JOIN Raspodela ON Osoba.id = nastavnik_id ");
+            naredba.Append(" WHERE godina_id = " + cmb_Godina.SelectedValue.ToString());
+            //textBox2.Text = naredba.ToString();
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza);
+            DataTable dt_profesor = new DataTable();
+            adapter.Fill(dt_profesor);
+            cmb_Profesor.DataSource = dt_profesor;
+            cmb_Profesor.ValueMember = "id";
+            cmb_Profesor.DisplayMember = "naziv";
+            cmb_Profesor.SelectedIndex = -1;
+        }
+
+        private void cmb_Profesor_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmb_Profesor.IsHandleCreated && cmb_Profesor.Focused)
+            {
+                cmb_PredmetPopulate();
+                cmb_Predmet.Enabled = true;
+            }
+        }
+
+        private void cmb_PredmetPopulate()
+        {
+            SqlConnection veza = Konekcija.Connect();
+            StringBuilder naredba = new StringBuilder("SELECT DISTINCT Predmet.id AS id, naziv FROM Predmet ");
+            naredba.Append(" JOIN Raspodela ON Predmet.id = predmet_id ");
+            naredba.Append(" WHERE godina_id = " + cmb_Godina.SelectedValue.ToString());
+            naredba.Append(" AND nastavnik_id = " + cmb_Profesor.SelectedValue.ToString());
+            //textBox2.Text = naredba.ToString();
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza);
+            DataTable dt_predmet = new DataTable();
+            adapter.Fill(dt_predmet);
+            cmb_Predmet.DataSource = dt_predmet;
+            cmb_Predmet.ValueMember = "id";
+            cmb_Predmet.DisplayMember = "naziv";
+            cmb_Predmet.SelectedIndex = -1;
+        }
+
+        private void cmb_Predmet_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmb_Predmet.IsHandleCreated && cmb_Predmet.Focused)
+            {
+                cmb_OdeljenjePopulate();
+                cmb_Odeljenje.Enabled = true;
+            }
+        }
+
+        private void cmb_OdeljenjePopulate()
+        {
+            StringBuilder naredba = new StringBuilder("SELECT DISTINCT Odeljenje.id AS id, STR(razred) + '-' + indeks AS naziv FROM Odeljenje ");
+            naredba.Append(" JOIN Raspodela ON Odeljenje.id = odeljenje_id ");
+            naredba.Append(" WHERE Raspodela.godina_id = " + cmb_Godina.SelectedValue.ToString());
+            naredba.Append(" AND nastavnik_id = " + cmb_Profesor.SelectedValue.ToString());
+            naredba.Append(" AND predmet_id = " + cmb_Predmet.SelectedValue.ToString());
+            //textBox2.Text = naredba.ToString();
+            SqlConnection veza = Konekcija.Connect();
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba.ToString(), veza);
+            DataTable dt_odeljenje = new DataTable();
+            adapter.Fill(dt_odeljenje);
+            cmb_Odeljenje.DataSource = dt_odeljenje;
+            cmb_Odeljenje.ValueMember = "id";
+            cmb_Odeljenje.DisplayMember = "naziv";
+            cmb_Odeljenje.SelectedIndex = -1;
+        }
+    }
+}
